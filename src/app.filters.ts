ee1566b8fb988d52ee54b41db148fdfed9459729
Sylvain T <garvek@gmail.com>
2023-08-13 12:11:46 +0200

    Remove StaticAsset module to allow a simple error handler (avoid throws E_NOENT)

---
+++ src/app.filters.ts
@@ -0,0 +1,23 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(_exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const httpStatus = _exception.getStatus();
+
+    response.render('error.html.twig', {
+      status: httpStatus,
+      path: request.url,
+    });
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './app.filters';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
